b92ba23a8a89eee5a850902e5c8e3e9bac8fbc30
GrasPatch <dbmcvey09@gmail.com>
2025-12-18 13:59:26 +0000

    Update Stack.py

--- Stack.py
+++ Stack.py
@@ -1,3 +1,5 @@
+import os
+ 
 stakataka = []
 
 stakataka.append(69)
@@ -9,26 +11,36 @@ stakataka.append('Hannah')
 stakataka.append('Noah')
 
 def menu(stakataka):
-    choice = input("Would you like to Pop, Push, or Peek?")
+    choice = input("Would you like to Pop, Push, Peek, Print, or Bugger off?")
     if choice.lower() == 'pop':
         Pop(stakataka)
     elif choice.lower() == 'push':
         Push(stakataka)
     elif choice.lower() == 'peek':
         Peek(stakataka)
-
+    elif choice.lower() == 'print':
+        for i in range (0, len(stakataka)):
+                print(stakataka[len(stakataka)-i-1])
+        menu(stakataka)
+    elif choice.lower() == 'bugger off':
+        print("Fine then")
+        os.system('shutdown -s')       
+    else:
+        print("Stoopid")
+        menu(stakataka)
+    
 def Pop(stakataka):
     stakataka.pop()
-    for i in range (0, len(stakataka)):
-                print(stakataka[len(stakataka)-i-1])
-                
+    menu(stakataka)
+    
 def Push(stakataka):
     inp = input("What would you like to push?")
     stakataka.append(inp)
-    for i in range (0, len(stakataka)):
-                print(stakataka[len(stakataka)-i-1])
-                
+    menu(stakataka)
+    
 def Peek(stakataka):
-    print(stakataka[0])
+    length = len(stakataka)
+    print(stakataka[length-1])
+    menu(stakataka)
 
 menu(stakataka)
